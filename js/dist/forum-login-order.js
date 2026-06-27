fe8c511235314eb3f06fb1ae690df5329178e952
ElfTorg <elftorg@gmail.com>
2026-06-27 19:38:10 +0300

    Load OAuth login order runtime patch

---
+++ js/dist/forum-login-order.js
@@ -0,0 +1,26 @@
+(function () {
+  function defaultExport(module) {
+    return module && module.__esModule ? module.default : module;
+  }
+
+  var app = flarum.reg.get('core', 'forum/app');
+  var extension = flarum.reg.get('core', 'common/extend');
+  var LogInButtons = defaultExport(flarum.reg.get('core', 'forum/components/LogInButtons'));
+  var LogInButton = defaultExport(flarum.reg.get('core', 'forum/components/LogInButton'));
+
+  app.initializers.add('fof/oauth-login-order', function () {
+    extension.extend(LogInButton, 'initAttrs', function (_, attrs) {
+      if (attrs.className && attrs.className.indexOf('FoFLogInButton') !== -1 && attrs.className.indexOf('Button--block') === -1) {
+        attrs.className = attrs.className.replace('Button ', 'Button Button--block ');
+      }
+    });
+
+    extension.override('flarum/forum/components/LogInModal', 'body', function () {
+      return [m('div', { className: 'Form Form--centered' }, this.fields().toArray()), m(LogInButtons)];
+    });
+
+    extension.override('flarum/forum/components/SignUpModal', 'body', function () {
+      return [m('div', { className: 'Form Form--centered' }, this.fields().toArray()), !this.attrs.token && m(LogInButtons)];
+    });
+  });
+})();
